94d9c63c7029f4e34732b344726ea743642a031d
Tango <maxim.ant07@gmail.com>
2026-02-14 06:05:00 +0300

    Update 3714. Longest Balanced Substring II.cpp

--- 3714. Longest Balanced Substring II.cpp
+++ 3714. Longest Balanced Substring II.cpp
@@ -1,74 +1,73 @@
 class Solution {
 public:
-    int mono(const string& s){
-        if(s.empty()) return 0;
-        int cnt = 1;
-        int ans = 1;
-        for(int i = 1; i < (int)s.size(); i ++){
-            if(s[i] == s[i - 1]) cnt++;
-            else cnt = 1;
-            ans = max(ans, cnt);
-        }
-        return ans;
-    }
+int32_t single(std::string_view pString)
+{
+	if (pString.empty())
+		return 0;
+	int32_t counter = 1, result = 1;
+	for (size_t i = 1; i < pString.size(); ++i)
+	{
+		counter = pString[i] == pString[i - 1] ? counter + 1 : 1;
+		result = std::max(result, counter);
+	}
+	return result;
+}
+int32_t duo(std::string_view pString, char pFirst, char pSecond)
+{
+	std::unordered_map<int32_t, int32_t> differences;
+	differences[0] = -1;
+	int32_t result = 0;
+	int32_t delta = 0;
+	for (size_t i = 0; i < pString.size(); ++i)
+	{
+		if (pString[i] != pFirst && pString[i] != pSecond)
+		{
+			differences.clear();
+			differences[0] = i;
+			delta = 0;
+			continue;
+		}
 
-    int duo(const string& s, char c1, char c2){
-        map<int, int> pos;
-        pos[0] = -1;
-        int ans = 0;
-        int delta = 0;
-        for(int i = 0; i < (int)s.size(); i ++){
-            if(s[i] != c1 && s[i] != c2){
-                pos.clear();
-                pos[0] = i;
-                delta = 0;
-                continue;
-            }
-            if(s[i] == c1){
-                delta++;
-            }
-            else{
-                delta--;
-            }
-            if(pos.find(delta) != pos.end()){
-                ans = max(ans, i - pos[delta]);
-            }
-            else{
-                pos[delta] = i;
-            }
-        }
-        return ans;
-    }
+		delta += pString[i] == pFirst ? +1 : -1;
 
-    int trio(const string& s){
-        vector<int> cnt(3, 0);
+		if (differences.contains(delta))
+			result = std::max(result, static_cast<int32_t>(i - differences[delta]));
+		else
+			differences[delta] = i;
+	}
+	return result;
+}
 
-        map<vector<int>, int> pos;
-        pos[{0, 0}] = -1;
+int32_t trio(std::string_view pString)
+{
+	std::vector<int32_t> counter(3, 0);
 
-        int ans = 0;
+	std::map<std::vector<int32_t>, int32_t> differences;
+	differences[{0, 0}] = -1;
 
-        for(int i = 0; i < (int)s.size(); i++){
-            cnt[s[i] - 'a']++;
+	int32_t result = 0;
 
-            vector<int> key = {cnt[1] - cnt[0], cnt[2] - cnt[0]};
+	for (size_t i = 0; i < pString.size(); ++i)
+	{
+		counter[pString[i] - 'a']++;
 
-            if(pos.find(key) != pos.end()){
-                ans = max(ans, i - pos[key]);
-            }
-            else{
-                pos[key] = i;
-            }
-        }
-        return ans;
-    }
-    int longestBalanced(string s) {
-        return max({
-            mono(s),
-            duo(s, 'a', 'b'),
-            duo(s, 'a', 'c'),
-            duo(s, 'b', 'c'),
-            trio(s)
-        });
-    }
+		std::vector<int32_t> key = { counter[1] - counter[0], counter[2] - counter[0] };
+		
+		if (differences.contains(key))
+			result = std::max(result, static_cast<int32_t>(i - differences[key]));
+		else
+			differences[key] = i;
+	}
+
+	return result;
+}
+
+int32_t longestBalanced(std::string_view pString)
+{
+	return std::max({ single(pString),
+					  duo(pString, 'a', 'b'),
+					  duo(pString, 'b', 'c'),
+					  duo(pString, 'a', 'c'),
+					  trio(pString) });
+}
 };
